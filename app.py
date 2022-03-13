b5de96f2ef9062da82154d46baf3097722817082
Vernon Hughes <vernonhughes@dyn-160-39-32-178.dyn.columbia.edu>
2022-03-12 20:07:10 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,25 @@
+import logging
+from sqlalchemy import MetaData
+from sqlalchemy import create_engine
+from sqlalchemy.engine.url import URL
+from sqlalchemy.ext.declarative import declarative_base
+
+DATABASE = {
+   'drivername': 'sqlite',
+    #'host': 'localhost',
+    #'port': '5432',
+    #'username': 'vernonhughes',
+    #'password': 'J@Messi123(4)',
+   'database': '/app/app.db'
+}
+
+def drop_table(table_name):
+   engine = create_engine(URL(**DATABASE))
+   base = declarative_base()
+   metadata = MetaData(engine, reflect=True)
+   table = metadata.tables.get(table_name)
+   if table is not None:
+       logging.info(f'Deleting {table_name} table')
+       base.metadata.drop_all(engine, [table], checkfirst=True)
+
+drop_table('user')
