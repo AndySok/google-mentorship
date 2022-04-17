cba43bcda6cd8858ca0c61e7fa0cb582f0ae4f42
Vernon Hughes <vernonhughes@Vernons-MacBook-Air.local>
2022-04-17 17:44:26 -0400

    updates

---
+++ migrations/versions/a192bd3f5503_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: a192bd3f5503
+Revises: 537bb352de7a
+Create Date: 2022-04-17 16:29:12.566465
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a192bd3f5503'
+down_revision = '537bb352de7a'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
